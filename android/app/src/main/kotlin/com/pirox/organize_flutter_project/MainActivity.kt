a65d2dcc27fd6bc0619414db9ba8adb6a1ddde77
Md Rafat Jamader Maraz <razhossain201@gmail.com>
2020-08-22 20:38:25 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/pirox/organize_flutter_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pirox.organize_flutter_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:organize_flutter_project/src/app.dart';
+
+void main() {
+  runApp(DemoApp());
+}
