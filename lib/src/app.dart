80cf0bc98c61a3addbe7ca0a2c87d57e38f80851
Md Rafat Jamader Maraz <razhossain201@gmail.com>
2020-08-22 20:43:54 +0600

    added dummy files

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:organize_flutter_project/src/views/ui/home.dart';
 
 class DemoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: YourMainPage(),
+      home: Home(),
     );
   }
 }
